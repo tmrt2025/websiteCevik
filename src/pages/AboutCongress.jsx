eb47a6ab7c919d38b382b1a7ff939846897106d2
tmrt2025 <rctmysgl.60@gmail.com>
2025-11-03 20:46:09 +0300

    remove all files from repository - preparing for fresh upload

--- src/pages/AboutCongress.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AboutCongress() {
-  return (
-    <div>
-      <h1>About Congress</h1>
-    </div>
-  )
-}
--- src/pages/notFoundPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function notFoundPage() {
-  return (
-    <div>notFoundPage</div>
-  )
-}
